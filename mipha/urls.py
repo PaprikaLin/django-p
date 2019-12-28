b1378c14154a2cf5f23950dc67ddd1d8da04a806
Paprika <paprika0214@163.com>
2019-12-28 13:30:53 +0800

    使用通用视图，不过目前还感觉不到很有用...

--- mipha/urls.py
+++ mipha/urls.py
@@ -11,11 +11,12 @@ from . import views
 
 app_name = 'mipha'
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('form/', views.formtest, name='testform'),
     # 定义访问不同的地址返回不同的模板，
-    path('<str:author>/', views.author_poets, name='author_poets'),
+    path('<str:author>/', views.AuthorPoetsView.as_view(), name='author_poets'),
     # re_path(r'^author/(\w+)$', views.author_poets, name='author_poets')
-    path('<str:author>/<int:slug>/', views.poet_page, name='poet_page'),
+    path('<str:author>/<int:slug>/', views.PoetView.as_view(), name='poet_page'),
     # 测试表单
 ]
--- mipha/views.py
+++ mipha/views.py
@@ -3,27 +3,39 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 # Create your views here.
 from .models import Post
 from django.template import loader
+from django.views import generic
 
-
+'''
 # index函数用来获取所有文章，获取到post_lists里面并通过HTTPResponse输出到客户端页面中
 def index(request):
     posts = Post.objects.all()
     #post_lists = list()
     #template = loader.get_template('mipha/index.html')
     context = {'posts': posts}
-    '''
-    硬编码。最好把html和python代码区分开来，使用render函数
-    for count, post in enumerate(posts):
-        # post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
-        post_lists.append("No.{}: {} {} {} <br>".format(str(count), str(post.author), str(post), str(post.pub_date)))
-    return HttpResponse(post_lists)
-    return render(posts, 'template/index.html')
-    
-    '''
+
+    # 硬编码。最好把html和python代码区分开来，使用render函数
+    # for count, post in enumerate(posts):
+    #     # post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+    #     post_lists.append("No.{}: {} {} {} <br>".format(str(count), str(post.author), str(post), str(post.pub_date)))
+    # return HttpResponse(post_lists)
+    # return render(posts, 'template/index.html')
+
 
     # render函数 (request, 模板路径 templates/mipha/index.html, 要传递的参数)
     # return render(request, 'mipha/index.html', context)
     return render(request, 'mipha/mainpage.html', context)
+'''
+
+class IndexView(generic.ListView):
+    model = Post
+    # 继承自ListView, 所以自动生成的变量是  post_list
+    # 如果需要修改，通过 context_object_name 属性
+    template_name = 'mipha/mainpage.html'
+
+
+    def get_queryset(self):
+        # 应该是用来读取数据库的函数
+        return Post.objects.order_by('-pub_date')
 
 
 # 定义链接应该返回的数据
@@ -31,16 +43,52 @@ def index(request):
 #   1. 从数据库获取数据
 #   2. 逻辑判断或者其他需要的事情
 #   3. 渲染模板，并且传递参数（目前看来是以字典的键与HTML里面的 {{ }}对应）
-def author_poets(request, author):
-    # poets = Post.objects.filter(author=author)
-    poets = get_list_or_404(Post, author=author)
-    context = {'author': author, 'poets': poets}
-    return render(request, 'mipha/author_filter.html', context)
+# def author_poets(request, author):
+#     # poets = Post.objects.filter(author=author)
+#     poets = get_list_or_404(Post, author=author)
+#     context = {'author': author, 'poets': poets}
+#     return render(request, 'mipha/author_filter.html', context)
+
+class AuthorPoetsView(generic.ListView):
+    model = Post
+    template_name = 'mipha/author_filter.html'
+    context_object_name = 'poets'
+
+    # 从请求路径中获取参数的方法  self.kwargs
+    # self.kwargs 是一个列表，键是在app/urls.py里面定义的路径的名称，这里是author
+    # 值是实际的输入，被<str:author>获取到的 "王维"
+    def get_queryset(self):
+        self.poets = get_list_or_404(Post, author=self.kwargs['author'])
+        # context = {'poets': Post.objects.filter(author=self.kwargs['author']), 'author':self.kwargs['author']}
+        return Post.objects.filter(author=self.kwargs['author'])
+
+    # 如果要传递额外的参数，要使用get_context_data函数
+    # 先通过super取出父类中的 上下文 也就是所有参数——可以看context的打印内容——然后再覆盖或者增加内容
+    def get_context_data(self, *, object_list=None, **kwargs):
+        print(self.kwargs)
+        context = super().get_context_data()
+        context['author'] = self.kwargs['author']
+        return context
+
+# def poet_page(request, author, slug):
+#     poet = get_object_or_404(Post, author=author, slug=slug)
+#     return render(request, 'mipha/poet.html', {'poet': poet})
 
 
-def poet_page(request, author, slug):
-    poet = get_object_or_404(Post, author=author, slug=slug)
-    return render(request, 'mipha/poet.html', {'poet': poet})
+class PoetView(generic.DetailView):
+    model = Post
+    template_name = 'mipha/poet.html'
+
+    # def get_context_data(self, **kwargs):
+    #     print('context  ', self.kwargs)
+    #     context = super().get_context_data()
+    #     print(context)
+    #     print(self.object)
+    #     return context
+
+    def get_queryset(self):
+        print('queryset   ',self.kwargs)
+        return Post.objects.filter(author=self.kwargs['author'], slug=self.kwargs['slug'])
 
 
 def formtest(request):
